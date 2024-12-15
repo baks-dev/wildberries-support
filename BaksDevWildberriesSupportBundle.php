34d9f22c7a649de289321c1315c4835c8edca5b4
kingstar <admin@baks.dev>
2024-12-15 23:21:46 +0300

    Baks Development

--- BaksDevWildberriesSupportBundle.php
+++ BaksDevWildberriesSupportBundle.php
@@ -31,22 +31,8 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class BaksDevWildberriesSupportBundle extends AbstractBundle
 {
-    public const NAMESPACE = __NAMESPACE__.'\\';
+    public const string NAMESPACE = __NAMESPACE__.'\\';
 
-    public const PATH = __DIR__.DIRECTORY_SEPARATOR;
+    public const string PATH = __DIR__.DIRECTORY_SEPARATOR;
 
-    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
-    {
-        $services = $container->services()
-            ->defaults()
-            ->autowire()
-            ->autoconfigure();
-
-        $services->load(self::NAMESPACE, self::PATH)
-            ->exclude([
-                self::PATH.'{Entity,Resources,Type}',
-                self::PATH.'**/*Message.php',
-                self::PATH.'**/*DTO.php',
-            ]);
-    }
 }
