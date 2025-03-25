2272b7ae457bd3b08f85713789610d358f18c4cc
kingstar <admin@baks.dev>
2025-03-25 21:00:49 +0300

    BaksDevelopment

--- Messenger/ReplyToReview/AutoReplyWbReviewDispatcher.php
+++ Messenger/ReplyToReview/AutoReplyWbReviewDispatcher.php
@@ -25,7 +25,7 @@ declare(strict_types=1);
 
 namespace BaksDev\Wildberries\Support\Messenger\ReplyToReview;
 
-use DateTimeImmutable;
+use BaksDev\Support\Answer\Service\AutoMessagesReply;
 use BaksDev\Support\Entity\Support;
 use BaksDev\Support\Repository\SupportCurrentEvent\CurrentSupportEventRepository;
 use BaksDev\Support\Type\Status\SupportStatus;
@@ -35,6 +35,7 @@ use BaksDev\Support\UseCase\Admin\New\Message\SupportMessageDTO;
 use BaksDev\Support\UseCase\Admin\New\SupportDTO;
 use BaksDev\Support\UseCase\Admin\New\SupportHandler;
 use BaksDev\Wildberries\Support\Type\WbReviewProfileType;
+use DateTimeImmutable;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\Attribute\Target;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
@@ -91,94 +92,31 @@ final readonly class AutoReplyWbReviewDispatcher
             return;
         }
 
-        // формируем сообщение в зависимости от условий отзыва
         $reviewRating = $message->getRating();
-
-        $hello = [
-            'Благодарим Вас за то, что выбрали наш магазин для покупки!',
-            'Спасибо, что выбрали наш магазин для покупки!',
-            'Спасибо, что выбрали именно нас!',
-            'Благодарим вас за то, что остановили свой выбор на нашем магазине!',
-            'Спасибо, что сделали выбор в пользу нашего магазина!',
-        ];
-
-        $key = array_rand($hello, 1);
-        $hello = $hello[$key];
-
 
         /**
          * Текст сообщения в зависимости от рейтинга
+         * по умолчанию текс с высоким рейтингом, 5 «HIGH»
          */
 
-        if($reviewRating === 5)
-        {
-            $answerMessage = null;
-            $answerMessage[] = 'Мы ценим Ваше доверие и всегда стремимся предоставить лучший сервис и продукт высокого качества.';
-            $answerMessage[] = 'Мы стремимся предоставить только лучшее, и ваша покупка подтверждает это.';
-            $answerMessage[] = 'Мы ценим вашу поддержку и уверены, что вы будете довольны своим приобретением.';
-            $answerMessage[] = 'Мы рады, что вы стали нашим клиентом, и надеемся, что ваш опыт с нашей продукцией будет исключительно положительным.';
-            $answerMessage[] = 'Мы уверены, что наша продукция оправдает ваши ожидания.';
-            $answerMessage[] = 'Мы уверены, что вы будете довольны качеством нашей продукции.';
-
-            $key = array_rand($answerMessage, 1);
-            $answerMessage = $answerMessage[$key];
-
-        }
+        $AutoMessagesReply = new AutoMessagesReply();
+        $answerMessage = $AutoMessagesReply->high();
 
         if($reviewRating === 4 || $reviewRating === 3)
         {
-            $answerMessage = null;
-            $answerMessage[] = 'Спасибо за ваш отзыв и высокую оценку! Мы рады узнать, что в целом вы остались довольны покупкой в нашем магазине.';
-            $answerMessage[] = 'Благодарим за обратную связь! Рады, что в целом вы остались довольны.';
-            $answerMessage[] = 'Благодарим за обратную связь! Мы работаем над улучшениями и надеемся, что в следующий раз вы поставите 5 звезд.';
-            $answerMessage[] = 'Рады, что в целом вам понравилось и вы остались довольны. Ваши замечания помогут нам стать лучше.';
-            $answerMessage[] = 'Спасибо за ваш отзыв! Мы рады, что в целом вы остались довольны, и всегда готовы к улучшениям.';
-
-            $key = array_rand($answerMessage, 1);
-            $answerMessage = $answerMessage[$key];
+            $answerMessage = $AutoMessagesReply->avg();
         }
-
 
         if($reviewRating < 3)
         {
-            $answerMessage = null;
-            $answerMessage[] = 'Извините за возникшие неудобства. Нам жаль, что вы остались недовольны.';
-            $answerMessage[] = 'Приносим искренние извинения за возможные неудобства, которые могло вызвать у вас.';
-            $answerMessage[] = 'Нам важно ваше мнение! Мы надеемся, что вы дадите нам еще один шанс. Приносим искренние извинения за возможные неудобства, которые могло вызвать у вас.';
-            $answerMessage[] = 'Мы надеемся, что вы позволите восстановить ваше доверие к нам. Приносим искренние извинения за возможные неудобства, которые могло вызвать у вас.';
-            $answerMessage[] = 'Нам жаль, что вы остались недовольны. Извините за возникшие неудобства. Надеемся, вы вернетесь, чтобы увидеть наши изменения.';
-            $answerMessage[] = 'Мы ценим ваш отзыв и будем работать над улучшениями. Извините за возникшие неудобства.';
-            $answerMessage[] = 'Приносим извинения за возможные неудобства. Мы надеемся на возможность сделать ваш следующий визит лучше.';
-            $answerMessage[] = 'Надеемся, вы вернетесь, чтобы увидеть наши улучшения. Нам жаль, что вы остались недовольны.';
-
-            $key = array_rand($answerMessage, 1);
-            $answerMessage = $answerMessage[$key];
+            $answerMessage = $AutoMessagesReply->low();
         }
 
-        /**
-         * Прощальный текст
-         */
-
-        $goodbye = [
-            'Ждем вас в нашем магазине вновь!',
-            'Будем рады видеть вас снова!',
-            'Всего хорошего! Ждем вас в следующий раз!',
-            'Нам будет приятно вас снова увидеть!',
-            'Всегда рады вам! Увидимся в следующий раз!',
-            'Спасибо за ваше время! Ждем вас снова!',
-            'Мы ценим ваше мнение и ждем вас снова!',
-            'Мы будем рады вашему возвращению!',
-        ];
-
-        $key = array_rand($goodbye, 1);
-        $goodbye = $goodbye[$key];
-
-
         /** Отправляем сообщение клиенту */
-        /** @var string $answerMessage */
+
         $supportMessageDTO = new SupportMessageDTO()
             ->setName('admin (WB Seller)')
-            ->setMessage($hello.PHP_EOL.$answerMessage.PHP_EOL.$goodbye)
+            ->setMessage($answerMessage)
             ->setDate(new DateTimeImmutable('now'))
             ->setOutMessage();
 
